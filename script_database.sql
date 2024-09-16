77af19711436ca0c48c4f9111c8bf8e26bb96412
Erick-Web09 <erickparedespilco@gmail.com>
2024-09-16 17:15:06 -0500

    File script

--- script_database.sql
+++ script_database.sql
@@ -1,2 +1,17 @@
 CREATE DATABASE dsrp_prestamos_financieros;
 GO
+
+USE dsrp_prestamos_financieros;
+GO
+
+CREATE TABLE personas_naturales (
+id INT PRIMARY KEY IDENTITY (1,1),
+numero_documento VARCHAR(15) UNIQUE NOT NULL,
+nombres VARCHAR(255) NOT NULL,
+apellido_paterno VARCHAR(255) NOT NULL,
+apellido_materno VARCHAR(255) NOT NULL,
+direccion NVARCHAR(1000) NOT NULL,
+celular VARCHAR(15) NOT NULL,
+email VARCHAR(255) NOT NULL
+)
+
